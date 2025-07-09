48e476dbe381328a0b2bc05b5ce5a8b0c009afc9
ihor_hozhenko <lugtrauma@gmail.com>
2025-07-09 19:42:20 +0100

    Added Navigation

--- src/app/components/sidebar-item.tsx
+++ src/app/components/sidebar-item.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import clsx from 'clsx';
 
 export interface SidebarItemProps {
+  current?: boolean;
   pathname: string;
   src: string;
   alt: string;
@@ -9,17 +12,25 @@ export interface SidebarItemProps {
 }
 
 export default function SidebarItem({
-  pathname,
-  src,
-  alt,
-  children,
-}: SidebarItemProps) {
+                                      current,
+                                      pathname,
+                                      src,
+                                      alt,
+                                      children,
+                                    }: SidebarItemProps) {
   return (
-    <li>
-      <a href={pathname} className="flex items-center h-9 mx-1 gap-3.5">
-        <Image className="ml-5" width={18} height={18} src={src} alt={alt} />
-        <span className="font-medium text-zinc-50">{children}</span>
-      </a>
-    </li>
+      <li>
+        <Link
+            href={pathname}
+            className={clsx(
+                'flex items-center h-9 mx-1 gap-3.5',
+                current &&
+                'after:h-full after:ml-auto after:border-2 after:border-purple-200 after:rounded-sm',
+            )}
+        >
+          <Image className="ml-5" width={18} height={18} src={src} alt={alt} />
+          <span className="font-medium text-zinc-50">{children}</span>
+        </Link>
+      </li>
   );
 }
