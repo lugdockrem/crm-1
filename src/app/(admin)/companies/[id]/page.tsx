41d0ea35da7c3b469ee27c4ed904151de4a35cd1
ihor_hozhenko <lugtrauma@gmail.com>
2025-07-21 18:56:19 +0100

    Added Parallel Routes

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,23 +1,14 @@
-'use client';
-
-import React, { useEffect } from 'react';
+import React from 'react';
 import Header from '@/app/components/header';
-import { notFound } from 'next/navigation';
 
 export interface PageProps {
-  params: { id: string };
+    params: { id: string };
 }
 
 export default function Page({ params }: PageProps) {
-    useEffect(() => {
-        const id = Number.parseInt(params.id)
-        if (Number.isNaN(id)) {
-            notFound();
-        }
-    }, [params.id]);
-  return (
-    <>
-      <Header>Companies ({params.id})</Header>
-    </>
-  );
+    return (
+        <>
+            <Header>Company ({params.id})</Header>
+        </>
+    );
 }
--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import Header from '@/app/components/header';
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
     return (
-        <main>
-            <h1 className="text-xl">Dashboard page</h1>
-          <MagicButton />
-        </main>
+        <>
+            <Header>Dashboard</Header>
+        </>
     );
 }
