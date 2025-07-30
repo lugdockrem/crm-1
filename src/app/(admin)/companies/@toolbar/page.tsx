d3f42bf3970390ce5577ed3791afc7d78b4ccdbc
ihor_hozhenko <lugtrauma@gmail.com>
2025-07-30 18:12:24 +0100

    Added React Query

--- src/app/(admin)/companies/@toolbar/page.tsx
+++ src/app/(admin)/companies/@toolbar/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from '@/app/components/header';
 import Toolbar from '@/app/components/toolbar';
 import SearchInput from '@/app/components/search-input';
 import AddCompanyButton from '@/app/components/add-company-button';
@@ -7,9 +6,9 @@ import AddCompanyButton from '@/app/components/add-company-button';
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
-    return (
-        <Toolbar action={<AddCompanyButton />}>
-            <SearchInput />
-        </Toolbar>
-    );
+  return (
+    <Toolbar action={<AddCompanyButton />}>
+      <SearchInput />
+    </Toolbar>
+  );
 }
